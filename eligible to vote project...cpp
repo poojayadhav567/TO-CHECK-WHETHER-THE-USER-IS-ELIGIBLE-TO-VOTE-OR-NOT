6410938a05a8046f2b9408ec7663c7d7185e904f
poojayadhav567 <92743586+poojayadhav567@users.noreply.github.com>
2021-10-23 18:08:50 +0530

    Add files via upload
    TO CKECK WHETHER THE PERSON IS ELIGIBLE TO VOTE .

---
+++ eligible to vote project...cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"enter the age of the user : ";
+	cin>>age;
+	if (age>=18)
+	{
+		cout<<"\n  YOU ARE ELIGIBLE TO VOTE !!!";
+	}
+	else
+	{
+		cout<<"\n  YOU ARE  NOT ELIGIBLE TO VOTE !!!";
+	}
+}
+	
+	
